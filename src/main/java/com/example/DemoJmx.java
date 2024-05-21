2f51f370d2db30ca7d0dcfad7a14bd3ad9d43d20
Estêvão Saleme <estevaosaleme@users.noreply.github.com>
2024-05-21 18:09:22 -0300

    feat: add first commit

---
+++ src/main/java/com/example/DemoJmx.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import javax.management.*;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+public class DemoJmx implements DemoJmxMBean {
+
+    private int volume = 0;
+
+    @Override
+    public void increaseVolume() {
+        volume++;
+    }
+
+    @Override
+    public void decreaseVolume() {
+        volume--;
+    }
+
+    @Override
+    public int getVolume() {
+        return volume;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        DemoJmx demoService = new DemoJmx();
+        ObjectName name;
+        try {
+            name = new ObjectName("com.example.DemoJmx:type=DemoJmxMBean");
+            mbs.registerMBean(demoService, name);
+        } catch (MalformedObjectNameException | NotCompliantMBeanException | InstanceAlreadyExistsException | MBeanRegistrationException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Press ENTER to stop the application...");
+        System.in.read();
+    }
+}
---
+++ src/main/java/com/example/DemoJmxMBean.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public interface DemoJmxMBean {
+    void increaseVolume();
+    void decreaseVolume();
+    int getVolume();
+}
